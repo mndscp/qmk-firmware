f654345cca86c9b0bbdf267baac563a3405a604e
mndscp <81937645+mndscp@users.noreply.github.com>
2021-08-29 15:08:39 +0200

    Add RGB toggle

--- keyboards/preonic/keymaps/mndscp-2x2u/rules.mk
+++ keyboards/preonic/keymaps/mndscp-2x2u/rules.mk
@@ -5,6 +5,6 @@ COMBO_ENABLE       = yes
 CONSOLE_ENABLE     = no
 LEADER_ENABLE      = no
 MOUSEKEY_ENABLE    = no
-RGBLIGHT_ENABLE    = no
+RGBLIGHT_ENABLE    = yes
 UNICODE_ENABLE     = no
 VIA_ENABLE         = no
