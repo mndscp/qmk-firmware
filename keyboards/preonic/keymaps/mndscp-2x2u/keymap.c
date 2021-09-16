5186951dc36d871c20e18a2ef0d1d09d81131736
mndscp <81937645+mndscp@users.noreply.github.com>
2021-09-16 16:41:25 +0200

    Disable numrow

--- keyboards/preonic/keymaps/mndscp-2x2u/keymap.c
+++ keyboards/preonic/keymaps/mndscp-2x2u/keymap.c
@@ -140,11 +140,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // |------+------+------+------+------+------+------+------+------+------+------+------|
   // | Ctrl |  GUI |  Alt | Lower|     Nav     |    Space    | Raise| Left |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
-    LT_TAB,  KC_Q,    KC_L,    KC_C,    KC_P,    KC_K,    KC_J,    KC_F,    KC_U,    KC_Y,    KC_QUOT, KC_BSPC,
-    LT_CTRL, LT_A,    HA_R,    HC_S,    HS_T,    KC_G,    KC_M,    HS_N,    HC_E,    HA_I,    KC_O,    KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_W,    KC_D,    KC_V,    KC_B,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
-    KC_LCTL, KC_LGUI, KC_LALT, LOWER,        LT_SPC,         SFT_SPC,       RAISE,   KC_LEFT, KC_UP,   KC_RGHT
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______,      _______,        _______,       _______, _______, _______, _______
   ),
 
   // [LAYER_APT] = LAYOUT_preonic_2x2u(
